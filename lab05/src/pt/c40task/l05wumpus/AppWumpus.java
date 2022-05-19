daea6dd987543a3581a8362ee6975e3d22d1fa8b
Jonathan do Ouro <102101030+JonathanDoOuro@users.noreply.github.com>
2022-05-19 11:21:51 -0300

    Add files via upload

--- lab05/src/pt/c40task/l05wumpus/AppWumpus.java
+++ lab05/src/pt/c40task/l05wumpus/AppWumpus.java
@@ -1,6 +1,6 @@
 package pt.c40task.l05wumpus;
 
-import java.net.StandardSocketOptions;
+import java.util.Scanner;
 
 public class AppWumpus {
 
@@ -16,46 +16,51 @@ public class AppWumpus {
       Toolkit tk = Toolkit.start(arquivoCaverna, arquivoSaida, arquivoMovimentos);
       
       String cave[][] = tk.retrieveCave();
-      System.out.println("=== Caverna");
-      for (int l = 0; l < cave.length; l++) {
-         for (int c = 0; c < cave[l].length; c++)
-            System.out.print(cave[l][c] + ((c < cave[l].length-1) ? ", " : ""));
-         System.out.println();
-      }
-      
-      String movements = tk.retrieveMovements();
-      System.out.println("=== Movimentos");
-      System.out.println(movements);
 
       Montador montador = new Montador();
       Caverna caverna = new Caverna();
       montador.conecta(caverna);
       montador.setInstrucoes(cave);
       montador.monta();
+      Controle controle = new Controle(caverna.getHeroi());
 
-      System.out.println("=== Caverna Intermediaria");
-      char partialCave[][] = {
-         {'#', '#', 'b', '-'},
-         {'#', 'b', '-', '-'},
-         {'b', '-', '-', '-'},
-         {'p', '-', '-', '-'}
-      };
-      int score = -120;
-      char status = 'x'; // 'w' para venceu; 'n' para perdeu; 'x' intermediárias
-      tk.writeBoard(partialCave, score, status);
-      caverna.imprime();
-
-      System.out.println("=== Última Caverna");
-      char finalCave[][] = {
-         {'#', '#', 'b', '-'},
-         {'#', 'b', '#', 'f'},
-         {'b', '-', '-', 'w'},
-         {'#', '-', '-', '-'}
-      };
-      score = -1210;
-      status = 'n'; // 'w' para venceu; 'n' para perdeu; 'x' intermediárias
-      tk.writeBoard(finalCave, score, status);
-      
+      String movements = tk.retrieveMovements();
+      //movements = null;
+      if (movements != null) {
+    	  System.out.println("=== Movimentos");
+    	  System.out.println(movements);
+          caverna.imprime("Alcebiades", controle.getScore());
+    	  for(int i = 0; i < movements.length() && controle.getEstadoJogo(); i++){
+             controle.agir(movements.charAt(i));
+             tk.writeBoard(caverna.toMatriz(), controle.getScore(), controle.getcEstado());
+             if (movements.charAt(i) != 'k') {
+            	 caverna.imprime("Alcebiades", controle.getScore());
+             }
+             if(!controle.getEstadoJogo() && controle.getcEstado() == 'w') 
+            	 System.out.println("Você ganhou =D !!!");
+             else if(!controle.getEstadoJogo() && controle.getcEstado() == 'n')
+            	 System.out.println("Você perdeu =( ...");
+          }
+      }
+     else{
+         Scanner keyboard = new Scanner(System.in);
+         String player = keyboard.nextLine();
+         caverna.imprime(player, controle.getScore());
+         while(controle.getEstadoJogo()){
+            String command = keyboard.nextLine();
+            for(int i = 0; i < command.length(); i++){
+               controle.agir(command.charAt(i));
+               tk.writeBoard(caverna.toMatriz(), controle.getScore(), controle.getcEstado());
+               if (command.charAt(i) != 'k') {
+                  caverna.imprime(player, controle.getScore());
+               }
+               if(!controle.getEstadoJogo() && controle.getcEstado() == 'w') 
+            	   System.out.println("Você ganhou =D !!!");
+               else if(!controle.getEstadoJogo() && controle.getcEstado() == 'n') 
+            	   System.out.println("Você perdeu =( ...");
+            }
+         }
+      }
       tk.stop();
    }
 
--- lab05/src/pt/c40task/l05wumpus/Componente.java
+++ lab05/src/pt/c40task/l05wumpus/Componente.java
@@ -1,33 +1,85 @@
 package pt.c40task.l05wumpus;
 
+//import java.util.Random;
+
 public class Componente {
-    protected char caracter;
-    protected boolean vida;
-    protected boolean artefato;
-    Componente(char caracter) {
-        this.caracter = caracter;
-        this.vida = true;
-        this.artefato = false;
-    }
+	protected char caracter; 
+	protected boolean vida;
+	protected boolean artefato, flechaEquipada, ouroCapturado;
+	protected int l, c, qtdFlechas;
+	Caverna caverna;
+	
+	public Componente(char caracter, int l, int c, Caverna caverna) {
+		this.caracter = caracter;
+		this.l = l;
+		this.c = c;
+		this.vida = true;
+		if (caracter == 'P'){
+			this.artefato = true;
+			this.qtdFlechas = 1;
+		}
+		else {
+			this.artefato = false;
+		}
+		this.flechaEquipada = false;
+		this.ouroCapturado = false;
+		this.caverna = caverna;
+	}
+	
+	public void instanciarSecundarios() {
+		
+	}
+	
+	public boolean matouWumpus() {
+		return false;
+	}
+	
+	public void conectarCaverna(Caverna caverna) {
+		this.caverna = caverna;
+	}
 
-    public boolean temArtefato() {
-        return artefato;
-    }
+	public void setPosicao(int l, int c) {
+		this.l = l;
+		this.c = c;
+	}
+	
+	public Caverna getCaverna(){return this.caverna;}
+	
+	public int getLinha() { return this.l; };
+	
+	public int getColuna() { return this.c; };
+	
+	public boolean temArtefato() {
+		return artefato;
+	}
+	
+	public void setArtefato(boolean artefato) {
+		this.artefato = artefato;
+	}
+	
+	public boolean getVida() {
+		return vida;
+	}
+	
+	public void setVida(boolean vida) {
+		this.vida = vida;
+	}
+	
+	public char getCaracter() {
+		return caracter;
+	}
 
-    public void setArtefato(boolean artefato) {
-        this.artefato = artefato;
-    }
+	public void equiparFlecha() {return;}
+	public boolean isFlechaEquipada() {return false;}
 
-    public boolean getVida() {
-        return vida;
-    }
 
-    public void setVida(boolean vida) {
-        this.vida = vida;
-    }
+	public void capturarOuro() {return;}
+	public boolean isOuroCapturado() {return false;}
 
-    public char getCaracter() {
-        return caracter;
-    }
+	public int getQtdFlehas() { return 0;}
 
+	public void mover(char mov) {return;}
+
+	public boolean batalhar(){return false;}
+ 
 }
