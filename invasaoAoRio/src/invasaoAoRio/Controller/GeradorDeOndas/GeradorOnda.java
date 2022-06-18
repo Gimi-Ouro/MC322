293e36ccb39a8861a60e612904da1e3557737f6a
gimiczz <gimicaroni@gmail.com>
2022-06-18 14:32:06 -0300

    corrigindo gerador de onda

--- invasaoAoRio/src/invasaoAoRio/Controller/GeradorDeOndas/GeradorOnda.java
+++ invasaoAoRio/src/invasaoAoRio/Controller/GeradorDeOndas/GeradorOnda.java
@@ -25,15 +25,15 @@ public class GeradorOnda implements IgeradorDeOndas{
             int posicionamento, tipo;
             Random rand = new Random();
             for (int i = 0; i < quantidadeNavios; i++){
-            posicionamento = rand.nextInt(0, mapa.getMapa().length);
-            tipo = rand.nextInt(1, dificuldade+1); //tipo de navio que será gerado. A dificuldade pode ir de 1 a 3, dependendo de qual onda estamos
-            if(tipo == 1){
+            posicionamento = rand.nextInt(mapa.getMapa().length);
+            tipo = rand.nextInt(dificuldade); //tipo de navio que será gerado. A dificuldade pode ir de 1 a 3, dependendo de qual onda estamos
+            if(tipo == 0){
                 mapa.getMapa()[posicionamento][16].addBarco(new Navio(50, 1, true));
             }
-            else if(tipo == 2){
+            else if(tipo == 1){
                 mapa.getMapa()[posicionamento][16].addBarco(new Navio(50, 2, true));
             }
-            else if(tipo == 3){
+            else if(tipo == 2){
                 mapa.getMapa()[posicionamento][16].addBarco(new Navio(100, 1, true));
             }
             //não sei se essa função sozinha espera o tempo. Quando coloquei ela, o IntelliJ pediu pra colocar aquele throws InterruptedException ali em cima
